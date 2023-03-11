ee4f6444ef7913e91ac45c5edc2b42bd4f512b73
apple <areej.fatima2k@gmail.com>
2023-03-11 20:49:37 +0500

    client build

---
+++ client/build/precache-manifest.4104f19a245916f8858c822a2c5f45a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba63c3693c3487de28e0ef3e8ab6baf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86af26cc3726fdf0c842",
+    "url": "/static/css/main.ff78aefe.chunk.css"
+  },
+  {
+    "revision": "e9c8043de43d284dbc38",
+    "url": "/static/js/2.7621cf4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7621cf4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86af26cc3726fdf0c842",
+    "url": "/static/js/main.26988d9f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
